9287fd39acfb3674df212be22b7ff0ec2bb29357
TaMeR <gruessle@gmail.com>
2019-02-03 22:21:04 +0300

    test

--- doc.go
+++ doc.go
@@ -1,4 +1,6 @@
-// Package gonano provides an implementation of the Nano cryptocurrency. It
-// encapsulates all packages of the gonano project. See the 'nano' package for
+// Package gonano provides an implementation of the Nano cryptocurrency. 
+//It	encapsulates all packages of the gonano project. See the 'nano' package for
 // the actual Nano implementation.
 package gonano
+
+
